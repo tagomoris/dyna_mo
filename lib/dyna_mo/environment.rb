931514b52800076d04485018333ef9a1fa53fa0d
TAGOMORI Satoshi <tagomoris@gmail.com>
2014-10-14 00:10:50 +0900

    remove comment which does not make sense now

--- lib/dyna_mo/environment.rb
+++ lib/dyna_mo/environment.rb
@@ -5,12 +5,6 @@ module DynaMo
 end
 
 module Kernel
-  # define class A -> A.prepend(X) -> overwrite defs of A
-  #   => A.ancestors: [X, A, ...] (correct)
-
-  # define A -> A.prepend(X) -> define B < A
-  #   => B.ancestors: [B, X, A, ...] (correct)
-
   def dynamo_define(target_name_or_instance, default_context_name, &block)
     raise "block is not given for dynamo_define" unless block_given?
     # target_name_or_instance accepts module instance, to help programmer to avoid typo
